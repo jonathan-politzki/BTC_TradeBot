f03c89baa50425cfd077cb6ffad11de8fd563b33
Jonathan Politzki <91631593+jonathan-politzki@users.noreply.github.com>
2024-07-09 01:35:09 -0500

    nothing much

---
+++ bot.py
@@ -0,0 +1,24 @@
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sb
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from xgboost import XGBClassifier
+from sklearn import metrics
+
+import warnings
+warnings.filterwarnings('ignore')
+
+# now we load the data, thank you very much copilot, shit is good
+# OHLC(‘Open’, ‘High’, ‘Low’, ‘Close’) data from 17th July 2014 to 29th December 2022 which is for 8 years for the Bitcoin price.
+
+df = pd.read_csv('bitcoin.csv')
+df.head()
+
+df.shape
+

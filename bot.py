a4f4cdbb3c5b8fb7043a65634412064c391b1f41
Jonathan Politzki <91631593+jonathan-politzki@users.noreply.github.com>
2024-07-10 16:35:03 -0500

    finished

--- bot.py
+++ bot.py
@@ -8,11 +8,13 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
+from sklearn import metrics
+from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix
 from sklearn.metrics import accuracy_score  # Example of specific metric import from scikit-learn
 from xgboost import XGBClassifier
 
 import warnings
-warnings.filterwarnings('ignore')
+warnings.filterwarnings('ignore') 
 
 # now we load the data, thank you very much copilot, shit is good
 # OHLC(‘Open’, ‘High’, ‘Low’, ‘Close’) data from 17th July 2014 to 29th December 2022 which is for 8 years for the Bitcoin price.
@@ -77,11 +79,7 @@ splitted = filtered_df['date'].dt.strftime('%Y-%m-%d').str.split('-', expand=Tru
 filtered_df['year'] = splitted[0].astype(int)
 filtered_df['month'] = splitted[1].astype(int)
 filtered_df['day'] = splitted[2].astype(int)
-
-filtered_df.head()
-
 filtered_df['is_quarter_end'] = np.where(filtered_df['month']%3==0,1,0)
-
 filtered_df['open-close']  = filtered_df['open'] - filtered_df['close']
 filtered_df['low-high']  = filtered_df['low'] - filtered_df['high']
 
@@ -91,3 +89,43 @@ filtered_df['target'] = np.where(filtered_df['close'].shift(-1) > filtered_df['c
 
 print(filtered_df.head())
 
+# correlation mapping shows us that the date and OHLC are super correlated obviously. Test this later.
+
+#sb.heatmap(filtered_df.corr() > 0.9, annot=True, cbar=False)
+#plt.show()
+
+# now we normalize the data
+
+model_features = filtered_df[['open-close', 'low-high', 'is_quarter_end']]
+target = filtered_df['target']
+
+scaler = StandardScaler()
+model_features = scaler.fit_transform(model_features)
+
+X_train, X_valid, Y_train, Y_valid = train_test_split(
+    model_features, target, test_size=0.1, random_state=2022)
+print(X_train.shape, X_valid.shape)
+
+# now we are testing the models and stuff
+
+models = [LogisticRegression(), SVC(kernel='poly', probability=True), XGBClassifier()]
+ 
+for i in range(3):
+  models[i].fit(X_train, Y_train)
+ 
+  print(f'{models[i]} : ')
+  print('Training Accuracy : ', metrics.roc_auc_score(Y_train, models[i].predict_proba(X_train)[:,1]))
+  print('Validation Accuracy : ', metrics.roc_auc_score(Y_valid, models[i].predict_proba(X_valid)[:,1]))
+  print()
+
+# lastly, a confusion matrix
+
+y_pred = models[0].predict(X_valid)
+
+# Create ConfusionMatrixDisplay
+disp = ConfusionMatrixDisplay.from_predictions(y_true=Y_valid, y_pred=y_pred)
+
+# Plot confusion matrix
+disp.plot()
+plt.title('Confusion Matrix')
+plt.show()
